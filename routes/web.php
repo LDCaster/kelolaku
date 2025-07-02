1c240401fc1fa19c0738d7e60cc92c466ebbdbb8
LDCaster <devoners9@gmail.com>
2025-07-03 07:17:05 +0800

    add : layout & sidebar edit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::get('/', function () {
     return view('/layout/main');
